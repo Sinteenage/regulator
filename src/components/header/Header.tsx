c4ca9ca9b8f2dda45ad3fefcb60794f9eaa2bf30
Sinteenage <sinteenage@gmail.com>
2023-04-11 20:04:30 +0300

    init regulator project

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import classes from './header.module.scss';
+
+export const Header: React.FC = () => {
+
+    return (
+        <header className={classes.header}>
+            
+        </header>
+    );
+};
